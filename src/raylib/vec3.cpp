ef899254fd67f9d0780136ecf34754e98b1a4ee3
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2018-08-27 05:58:03 +0100

    Implement assignment and unary operators for Vec3

--- src/raylib/vec3.cpp
+++ src/raylib/vec3.cpp
@@ -5,12 +5,41 @@ namespace raylib {
     Vec3::Vec3(float scalar) : x(scalar), y(scalar), z(scalar) {}
     Vec3::Vec3(float x_, float y_, float z_) : x(x_), y(y_), z(z_) {}
 
-    Vec3 &Vec3::operator+=(Vec3 other) { return *this; }
-    Vec3 &Vec3::operator-=(Vec3 other) { return *this; }
-    Vec3 &Vec3::operator*=(float scalar) { return *this; }
-    Vec3 &Vec3::operator/=(float scalar) { return *this; }
-    Vec3 Vec3::operator+() { return {}; }
-    Vec3 Vec3::operator-() { return {}; }
+    Vec3 &Vec3::operator+=(Vec3 other) { 
+        x += other.x;
+        y += other.y;
+        z += other.z;
+        return *this;
+    }
+
+    Vec3 &Vec3::operator-=(Vec3 other) { 
+        x -= other.x;
+        y -= other.y;
+        z -= other.z;
+        return *this;
+    }
+
+    Vec3 &Vec3::operator*=(float scalar) { 
+        x *= scalar;
+        y *= scalar;
+        z *= scalar;
+        return *this;
+    }
+
+    Vec3 &Vec3::operator/=(float scalar) { 
+        x /= scalar;
+        y /= scalar;
+        z /= scalar;
+        return *this;
+    }
+
+    Vec3 Vec3::operator+() { 
+        return Vec3(x, y, z); 
+    }
+
+    Vec3 Vec3::operator-() { 
+        return Vec3(-x, -y, -z); 
+    }
 
     bool operator==(Vec3 lhs, Vec3 rhs) { return false; }
     bool operator!=(Vec3 lhs, Vec3 rhs) { return false; }
