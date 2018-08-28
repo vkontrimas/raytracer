fa801e482824e14c9043652afee921f18492b551
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2018-08-28 14:19:26 +0100

    Write a test image

--- src/rayexe/main.cpp
+++ src/rayexe/main.cpp
@@ -1,3 +1,21 @@
+#include <raylib/image.hpp>
+#include "io.hpp"
+
+using raylib::Image;
+using raylib::Color;
+
 int main() {
+    Image image(256, 256);
+    for (int y = 0; y < image.height(); ++y) {
+        for (int x = 0; x < image.width(); ++x) {
+            float r = (1.0f / 256) * y;
+            float g = (1.0f / 256) * x;
+            Color color(r, g, 0.0f);
+            image.pixelAt(x, y) = color;
+        }
+    }
+
+    rayexe::writePNG("test.png", image);
+
     return 0;
 }
