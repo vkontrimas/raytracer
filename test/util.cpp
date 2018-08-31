99a464856a91b42f0bb0fba5d29875962f765cb6
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2018-08-31 22:05:49 +0100

    Add NaN and infinity tests for raylib::approx()

--- test/util.cpp
+++ test/util.cpp
@@ -54,4 +54,50 @@ TEST_CASE("Util raylib::approx()", "[util][math]") {
             CHECK_FALSE(raylib::approx(element.number, element.comparison, epsilon));
         }
     }
+
+    SECTION("NaNs and infinities") {
+        std::vector<float> data {
+            -2411.0f
+            -1000.0f,
+            -10.0f,
+            -1.0f,
+            -0.1f,
+            0.0f,
+            0.1f,
+            1.0f,
+            10.0f,
+            1000.0f,
+            2411.0f
+        };
+
+        SECTION("Infinities") {
+            float infinity = std::numeric_limits<float>::infinity();
+
+            SECTION("Positive") {
+                for (float num : data) {
+                    INFO(num << " is not approximately infinity");
+                    CHECK_FALSE(raylib::approx(num, infinity, epsilon));
+                }
+            }
+            CHECK(raylib::approx(infinity, infinity, epsilon));
+
+            SECTION("Negative") {
+                for (float num : data) {
+                    INFO(num << " is not approximately -infinity");
+                    CHECK_FALSE(raylib::approx(num, -infinity, epsilon));
+                }
+            }
+            CHECK(raylib::approx(-infinity, -infinity, epsilon));
+        }
+
+        SECTION("NaNs") {
+            float NaN = std::numeric_limits<float>::quiet_NaN();
+            for (float num : data) {
+                INFO(num << "is not approximately a NaN");
+                CHECK_FALSE(raylib::approx(num, NaN, epsilon));
+            }
+            // NaN comparisons are always false.
+            CHECK_FALSE(raylib::approx(NaN, NaN, epsilon));
+        }
+    }
 }
