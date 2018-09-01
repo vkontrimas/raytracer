fee914a67a667e266a132108021a387de1fb754a
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2018-09-01 09:56:15 +0100

    Implement sphere hit detection

--- src/raylib/surfaces/sphere.cpp
+++ src/raylib/surfaces/sphere.cpp
@@ -1,10 +1,20 @@
 #include <raylib/surfaces/sphere.hpp>
+#include <iostream>
 
 namespace raylib {
     Sphere::Sphere(Vec3 position, float radius) : m_position(position), m_radius(radius) {}
 
     HitInfo Sphere::checkHit(Ray ray) {
-        return { false };
+        Vec3 e = m_position - ray.origin;
+        float a_l = e.dot(ray.direction);
+        if (a_l < 0.0f) { return { false }; } // If the dot product is negative, the ray is not going in the right direction.
+
+        float f_sqr = (m_radius * m_radius) - e.dot(e) + (a_l * a_l);
+        bool hit = f_sqr >= 0.0f;
+
+        // float distance = a_l - std::sqrt(f_sqr);
+
+        return { hit };
     }
 
     std::ostream &operator<<(std::ostream &stream, Sphere sphere) {
--- test/surfaces/sphere.cpp
+++ test/surfaces/sphere.cpp
@@ -16,3 +16,45 @@ TEST_CASE("Sphere initialization", "[surfaces][sphere][init]") {
         CHECK(sphere.radius() == 123.0f);
     }
 }
+
+TEST_CASE("Sphere intersection", "[surfaces][shere][math][ray]") {
+    SECTION("Axis aligned") {
+        Sphere sphere(Vec3(0.0f, 0.0f, -10.0f), 1.0f);
+        INFO(sphere);
+
+        SECTION("Hit") {
+            Ray ray(Vec3(0.0f, 0.0f, 0.0f), Vec3(0.0f, 0.0f, -1.0f));
+            INFO(ray);
+            HitInfo result = sphere.checkHit(ray);
+            CHECK(result.hit);
+        }
+
+        SECTION("Hit 2") {
+            Ray ray(Vec3(0.5f, 0.0f, 0.0f), Vec3(0.0f, 0.0f, -1.0f));
+            INFO(ray);
+            HitInfo result = sphere.checkHit(ray);
+            CHECK(result.hit);
+        }
+
+        SECTION("Scrape") {
+            Ray ray(Vec3(0.99999f, 0.0f, 0.0f), Vec3(0.0f, 0.0f, -1.0f));
+            INFO(ray);
+            HitInfo result = sphere.checkHit(ray);
+            CHECK(result.hit);
+        }
+
+        SECTION("Miss") {
+            Ray ray(Vec3(1.1f, 0.0f, 0.0f), Vec3(0.0f, 0.0f, -1.0f));
+            INFO(ray);
+            HitInfo result = sphere.checkHit(ray);
+            CHECK_FALSE(result.hit);
+        }
+
+        SECTION("Opposite direction") {
+            Ray ray(Vec3(0.0f, 0.0f, 0.0f), Vec3(0.0f, 0.0f, 1.0f));
+            INFO(ray);
+            HitInfo result = sphere.checkHit(ray);
+            CHECK_FALSE(result.hit);
+        }
+    }
+}
