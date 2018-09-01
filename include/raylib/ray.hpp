f63da2b2689cd6c6cb6149043adc9c79f38ae4df
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2018-09-01 01:20:43 +0100

    Add raylib::Ray

---
+++ include/raylib/ray.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef RAYTRACER_RAY_HPP
+#define RAYTRACER_RAY_HPP
+
+namespace raylib {
+    struct Ray {
+        Vec3 origin;
+        Vec3 direction;
+
+        Ray(Vec3 origin_, Vec3 direction_) : origin(origin_), direction(direction_) {}
+    };
+}
+
+#endif /* RAYTRACER_RAY_HPP */
