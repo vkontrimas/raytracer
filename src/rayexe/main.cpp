eabec28e2a849f7966bb150c4020fa8aeda20247
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2018-09-01 10:15:35 +0100

    Implement raytracing mock-up

--- src/rayexe/main.cpp
+++ src/rayexe/main.cpp
@@ -1,17 +1,40 @@
 #include <raylib/image.hpp>
+#include <raylib/surfaces/sphere.hpp>
+#include <vector>
+#include <iostream>
 #include "io.hpp"
 
-using raylib::Image;
-using raylib::Color;
+using namespace raylib;
 
 int main() {
     Image image(256, 256);
+    float cameraWorldUnit = 0.025f;
+
+    std::vector<Sphere> spheres {
+        Sphere(Vec3(0.0f, 0.0f, -10.0f)),
+        Sphere(Vec3(2.0f, 0.0f, -5.0f), 0.5f),
+    };
+
+    Color backgroundColor(0.25f, 0.5f, 0.8f, 1.0f);
+
     for (int y = 0; y < image.height(); ++y) {
         for (int x = 0; x < image.width(); ++x) {
-            float r = (1.0f / 256) * y;
-            float g = (1.0f / 256) * x;
-            Color color(r, g, 0.0f);
-            image.pixelAt(x, y) = color;
+            Vec3 rayOrigin {
+                (-image.width() / 2.0f) * cameraWorldUnit + x * cameraWorldUnit,
+                (-image.height() / 2.0f) * cameraWorldUnit + y * cameraWorldUnit,
+                0.0f
+            };
+
+            Ray ray(rayOrigin, Vec3(0.0f, 0.0f, -1.0f));
+
+            image.pixelAt(x, y) = backgroundColor;
+
+            for (auto &&sphere : spheres) {
+                HitInfo hit = sphere.checkHit(ray);
+                if (hit.hit) {
+                    image.pixelAt(x, y) = Color(1.0f, 0.0f, 0.0f);
+                }
+            }
         }
     }
 
