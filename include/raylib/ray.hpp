6d81e5789412a89fb4dd2d6697c29d2acc571e5e
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2018-09-01 09:29:16 +0100

    Add stream insertion operator to Ray

--- include/raylib/ray.hpp
+++ include/raylib/ray.hpp
@@ -3,6 +3,7 @@
 #define RAYTRACER_RAY_HPP
 
 #include <raylib/vec3.hpp>
+#include <ostream>
 
 namespace raylib {
     struct Ray {
@@ -11,6 +12,11 @@ namespace raylib {
 
         Ray(Vec3 origin_, Vec3 direction_) : origin(origin_), direction(direction_) {}
     };
+
+    std::ostream &operator<<(std::ostream &stream, Ray ray) {
+        stream << "[" << ray.origin << ", " << ray.direction << "]";
+        return stream;
+    }
 }
 
 #endif /* RAYTRACER_RAY_HPP */
