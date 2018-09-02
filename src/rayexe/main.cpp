4bc65970c20bc041edf4b611cc036c4189d4d7f4
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2018-09-03 00:29:09 +0100

    Use raylib::raytrace in rayexe

--- src/rayexe/main.cpp
+++ src/rayexe/main.cpp
@@ -1,42 +1,18 @@
-#include <raylib/image.hpp>
 #include <raylib/surfaces/sphere.hpp>
-#include <raylib/scene.hpp>
-#include <vector>
-#include <iostream>
+#include <raylib/raytrace.hpp>
 #include "io.hpp"
 
 using namespace raylib;
 
 int main() {
     Image image(256, 256);
-    float cameraWorldUnit = 0.025f;
 
     Scene scene;
     scene.addSurface(Sphere(Vec3(1.0f, -0.5f, -5.0f), 0.5f));
     scene.addSurface(Sphere(Vec3(0.0f, 0.0f, -10.0f)));
     scene.addSurface(Sphere(Vec3(1.0f, 0.0f, -15.0f), 0.5f));
 
-    Color backgroundColor(0.25f, 0.5f, 0.8f, 1.0f);
-
-    for (int y = 0; y < image.height(); ++y) {
-        for (int x = 0; x < image.width(); ++x) {
-            Vec3 rayOrigin {
-                (-image.width() / 2.0f) * cameraWorldUnit + x * cameraWorldUnit,
-                (-image.height() / 2.0f) * cameraWorldUnit + y * cameraWorldUnit,
-                0.0f
-            };
-
-            Ray ray(rayOrigin, Vec3(0.0f, 0.0f, -1.0f));
-
-            image.pixelAt(x, y) = backgroundColor;
-
-            HitInfo hit = scene.checkHit(ray);
-            if (hit) {
-                float distance = hit.position.magnitude();
-                image.pixelAt(x, y) = Color(distance * 0.05f);
-            }
-        }
-    }
+    raylib::raytrace(scene, Camera(Vec3(), Vec3(0.0f, 0.0f, -1.0f)), image);
 
     rayexe::writePNG("test.png", image);
 
