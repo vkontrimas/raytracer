f1950d47b2ca3b3d6d27dd686df4a8957a62dbee
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2018-09-03 00:10:11 +0100

    Add fov to Camera

--- include/raylib/camera.hpp
+++ include/raylib/camera.hpp
@@ -7,18 +7,21 @@
 namespace raylib {
     class Camera {
     public:
-        Camera(Vec3 position, Vec3 direction);
+        Camera(Vec3 position, Vec3 direction, float fov = 80.0f);
 
         Vec3 &position() { return m_position; }
         Vec3 &direction() { return m_direction; }
+        float &fov() { return m_fov; }
         const Vec3 &position() const { return m_position; }
         const Vec3 &direction() const { return m_direction; }
+        const float &fov() const { return m_fov; }
 
         void lookAt(Vec3 position);
 
     private:
         Vec3 m_position;
         Vec3 m_direction;
+        float m_fov;
     };
 }
 #endif /*RAYTRACER_CAMERA_HPP*/
--- src/raylib/camera.cpp
+++ src/raylib/camera.cpp
@@ -1,7 +1,7 @@
 #include <raylib/camera.hpp>
 
 namespace raylib {
-    Camera::Camera(Vec3 position, Vec3 direction) : m_position(position), m_direction(direction) {}
+    Camera::Camera(Vec3 position, Vec3 direction, float fov) : m_position(position), m_direction(direction), m_fov(fov) {}
 
     void Camera::lookAt(Vec3 targetPosition) {
         m_direction = (targetPosition - m_position).normalized();
