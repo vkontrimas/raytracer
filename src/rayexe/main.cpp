fb2229ce1d3b2744a35bca536358e79212443d83
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2018-09-06 12:36:45 +0100

    Benchmark

--- src/rayexe/main.cpp
+++ src/rayexe/main.cpp
@@ -2,9 +2,12 @@
 #include <raylib/scene.hpp>
 #include <raylib/camera.hpp>
 #include <raylib/image.hpp>
+#include <chrono>
+#include <iostream>
 #include "io.hpp"
 
 using namespace raylib;
+using benchclock = std::chrono::high_resolution_clock;
 
 int main() {
     Image image(728, 432);
@@ -21,7 +24,21 @@ int main() {
     scene.addObject(Object(Vec3(1.0f, 0.0f, -15.0f), &c));
     scene.addObject(Object(Vec3(0.0f, 0.0f, -10.0f), &d));
 
-    scene.raytrace(Camera(Vec3(), Vec3(0.0f, 0.0f, -1.0f)), image);
+    std::chrono::milliseconds totalTime = {};
+    const int numIterations = 10;
+    std::cout << "Image size: " << image.width() << " x " << image.height() << std::endl;
+    std::cout << "Iterations: " << numIterations << std::endl;
+    for (int i = 0; i < numIterations; ++i) {
+        auto start = benchclock::now();
+
+        scene.raytrace(Camera(Vec3(), Vec3(0.0f, 0.0f, -1.0f)), image);
+
+        auto end = benchclock::now();
+        std::chrono::milliseconds time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        totalTime += time;
+        std::cout << "[" << i << "/" << numIterations << "]" << "Scene::raytrace time: " << time.count() << "ms" << std::endl; 
+    }
+    std::cout << "Total: " << totalTime.count() << "ms\nAverage: " << totalTime.count() / numIterations << "ms" << std::endl;
 
     // NOTE: Writing twice for now, to make it easier to open the most recent image.
     rayexe::writePNG("latest.png", image);
