4fac610b3328e79b373ed9a9587aba7068a9c04b
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2018-09-06 10:36:39 +0100

    Implement proper perspective rendering

--- src/raylib/raytrace.cpp
+++ src/raylib/raytrace.cpp
@@ -1,25 +1,38 @@
 #include <raylib/raytrace.hpp>
+#include <raylib/mat4.hpp>
+#include <cmath>
+
+namespace {
+    const float deg2rad = 0.0174532f;
+}
 
 namespace raylib {
     void raytrace(const Scene &scene, const Camera &camera, Image &image) {
-        //TEMP: ALl of it. We need to account for camera orientation and vector rotation for persepective.
-        float step = 0.025f;
+        float aspectRatio = static_cast<float>(image.height()) / static_cast<float>(image.width());
+        float verticalFOV = camera.fov() * deg2rad;
+        float horizontalFOV = 2.0f * std::atan(std::tan(verticalFOV / 2.0f) * aspectRatio);
+        float stepX = (camera.fov() / image.height()) * deg2rad;
+        float stepY = (horizontalFOV / image.width());
+
+        Vec3 cameraRight = camera.right();
+        Vec3 cameraUp = camera.up();
+        Vec3 cameraForward = camera.forward();
+        Vec3 cameraPosition = camera.getPosition();
+
         Color backgroundColor(0.25f, 0.5f, 0.8f, 1.0f);
         for (int y = 0; y < image.height(); ++y) {
             for (int x = 0; x < image.width(); ++x) {
-                Vec3 rayOrigin {
-                    (-image.width() / 2.0f) * step + x * step,
-                    (-image.height() / 2.0f) * step + y * step,
-                    0.0f
-                };
-                Ray ray(rayOrigin, Vec3(0.0f, 0.0f, -1.0f));
+                Mat4 rotateX = Mat4::rotate(cameraRight, stepX * ((image.height() / 2) - y)); // Y = 0 is the top of the screen, so we need to move down
+                Mat4 rotateY = Mat4::rotate(cameraUp, stepY * (-(image.width() / 2) + x));
+
+                Vec3 rayDirection = rotateX * rotateY * cameraForward;
+                Ray ray(cameraPosition, rayDirection);
 
                 image.pixelAt(x, y) = backgroundColor;
 
                 HitInfo hit = scene.checkHit(ray);
                 if (hit) {
-                    float distance = hit.position.magnitude();
-                    image.pixelAt(x, y) = Color(distance * 0.05f);
+                    image.pixelAt(x, y) = Color(hit.normal.x, hit.normal.y, hit.normal.z);
                 }
             }
         }
