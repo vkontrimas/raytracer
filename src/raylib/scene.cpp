bef35341969d336e9732947439de5623ad67e813
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2018-09-06 14:36:21 +0100

    Improve perf a tiny bit

--- src/raylib/scene.cpp
+++ src/raylib/scene.cpp
@@ -12,22 +12,22 @@ namespace raylib {
     void Scene::raytrace(const Camera &camera, Image &image) const {
         float aspectRatio = static_cast<float>(image.width()) / static_cast<float>(image.height());
         float verticalFOV = camera.fov() * deg2rad;
-        float horizontalFOV = 2.0f * std::atan(std::tan(verticalFOV / 2.0f) * aspectRatio);
-        float stepX = (camera.fov() / image.height()) * deg2rad;
-        float stepY = (horizontalFOV / image.width());
 
-        Vec3 cameraRight = camera.right();
-        Vec3 cameraUp = camera.up();
-        Vec3 cameraForward = camera.forward();
+        float farHeight = 2.0f * std::tan(verticalFOV / 2.0f) * camera.drawDistance();
+        float stepY = -farHeight / image.height();
+        float startY = farHeight / 2.0f;
+
+        float farWidth = farHeight * aspectRatio;
+        float stepX = farWidth / image.width();
+        float startX = -farWidth / 2.0f;
+
+        Mat4 cameraTransform = camera.getTransform();
         Vec3 cameraPosition = camera.getPosition();
 
+        Vec3 rayDirection(startX, startY, -camera.drawDistance());
         for (int y = 0; y < image.height(); ++y) {
             for (int x = 0; x < image.width(); ++x) {
-                Mat4 rotateX = Mat4::rotate(cameraRight, stepX * ((image.height() / 2) - y)); // Y = 0 is the top of the screen, so we need to move down
-                Mat4 rotateY = Mat4::rotate(cameraUp, stepY * (-(image.width() / 2) + x));
-
-                Vec3 rayDirection = rotateX * rotateY * cameraForward;
-                Ray ray(cameraPosition, rayDirection);
+                Ray ray(cameraPosition, (cameraTransform * rayDirection).normalized());
 
                 HitInfo hit = {};
                 Object hitObject;
@@ -43,7 +43,10 @@ namespace raylib {
                 }
 
                 image.pixelAt(x, y) = hit ? hitObject.color : backgroundColor();
+                rayDirection.x += stepX;
             }
+            rayDirection.x = startX;
+            rayDirection.y += stepY;
         }
     }
 }
